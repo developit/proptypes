b025e5ed8d752607eae4a196960434d85a5092fe
Artem Sapegin <artem@sapegin.ru>
2017-03-09 18:11:11 +0100

    Add disabled version

---
+++ src/disabled.js
@@ -0,0 +1,23 @@
+function proptype() {}
+proptype.isRequired = proptype;
+
+const getProptype = () => proptype;
+
+export default {
+    array: proptype,
+    bool: proptype,
+    func: proptype,
+    number: proptype,
+    object: proptype,
+    string: proptype,
+    symbol: proptype,
+    any: getProptype,
+    arrayOf: getProptype,
+    element: getProptype,
+    instanceOf: getProptype,
+    node: getProptype,
+    objectOf: getProptype,
+    oneOf: getProptype,
+    oneOfType: getProptype,
+    shape: getProptype,
+};
